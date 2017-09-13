bf58c305a2bab07172e04d6898945c89b6cf45ae
Sven Broman <sven.d.broman@gmail.com>
2017-09-13 19:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string() === "I love you grandma.") {
+  if (string() === "I love you, grandma.") {
     return ("I love you too.")
   }
 }
